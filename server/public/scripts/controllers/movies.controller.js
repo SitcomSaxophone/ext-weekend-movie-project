613077ae1d9e56873a4344cd095cbdcd91e51bb9
juliq <juliannq@mac.com>
2018-09-29 15:52:38 -0500

    Get function for displaying movies on DOM works

--- server/public/scripts/controllers/movies.controller.js
+++ server/public/scripts/controllers/movies.controller.js
@@ -1,45 +1,47 @@
 app.controller('MovieController', ['$http', function($http) {
     console.log('Movie Controller loaded');
-    const vm = this;
+    var self = this;
 
-let vm = this;
+    self.movieArray = [];
 
-vm.movies = []
-
-// GET all movies
-vm.getMovies = function() {
-    $http.get('/movies')
-    .then(function(response) {
-        console.log('response', response);
-        vm.movies = response.data;
-    })
-}
-
-// ADD(POST) a movie to the database
-vm.addMovie = function (movie) {
-    console.log(movie);
-    let movieToAdd = { title: movies.title, release_date: movies.release_date, run_time: movies.run_time, image: movies.image, genre_id: movies.genre_id};
-    $http({
-        method: 'POST',
-        url: 'movies',
-        data: movieToAdd
-    }).then((response) => {
-        console.log('Error posting new listing:', error);
-    });
-    vm.getAllMovies();
-} // end addMovie
-
-vm.getAllMovies();
+    // GET all movies
+    self.getAllMovies = function () {
+        $http({
+            method: 'GET',
+            url: '/movies'
+        }).then(function (response) {
+            console.log('getAllMovies successful', response.data);
+            self.movieArray = response.data;
+        }).catch(function (error) {
+            console.log('error getting properties from database', error);
+        });
+    };
 
 
-// DELETE a movie from the database
-vm.deleteMovie = function(id) {
-    $http.delete(`/movies/`, {params : {id: id}})
-    .then(function() {
-        vm.getAllMovies()
-    })
-};
+    // ADD(POST) a movie to the database
+    self.addMovie = function (movie) {
+        console.log(movie);
+        let movieToAdd = { title: movies.title, release_date: movies.release_date, run_time: movies.run_time, image: movies.image, genre_id: movies.genre_id };
+        $http({
+            method: 'POST',
+            url: '/movies',
+            data: movieToAdd
+        }).then((response) => {
+            console.log('Error posting new listing:', error);
+        });
+        self.getAllMovies();
+    }; // end addMovie
 
 
-    
+    // DELETE a movie from the database
+    self.deleteMovie = function (id) {
+        $http.delete(`/movies/`, { params: { id: id } })
+            .then(function () {
+                self.getAllMovies();
+            })
+    };
+
+    self.getAllMovies();
+
+
 }]); // end MovieApp.controller
